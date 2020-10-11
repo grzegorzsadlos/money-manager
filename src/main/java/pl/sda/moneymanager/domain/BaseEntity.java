a99e6d14ca1ea8d07251cccd219876102d42189b
Grzegorz <grzegorzsadlos@wp.pl>
2020-10-11 12:01:57 +0200

    [fix] - entities mapping fixes

--- src/main/java/pl/sda/moneymanager/domain/BaseEntity.java
+++ src/main/java/pl/sda/moneymanager/domain/BaseEntity.java
@@ -4,13 +4,28 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDateTime creationTimestamp;
     private LocalDateTime updateTimestamp;
+
+    @PrePersist  //przed zapisaniem pierwszy raz
+    void setCreationTimestamp() {
+        creationTimestamp = LocalDateTime.now();
+        updateTimestamp = creationTimestamp;
+    }
+
+    @PreUpdate  //Przed aktualizacją
+    void setUpdateTimestamp(){
+        updateTimestamp = LocalDateTime.now();
+    }
 }
