441c5b1633befbcedff0d050d1da2cc7f132d41b
Grzegorz <grzegorzsadlos@wp.pl>
2020-10-18 12:40:45 +0200

    [add] Integration Controller & rest template

--- src/main/java/pl/sda/moneymanager/MoneyManagerApplication.java
+++ src/main/java/pl/sda/moneymanager/MoneyManagerApplication.java
@@ -2,12 +2,22 @@ package pl.sda.moneymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MoneyManagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MoneyManagerApplication.class, args);
+
+    }
+    @Bean
+    RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+        return restTemplateBuilder.build();
     }
 
+
 }
+
+
